c630bd5984ca24b1a2d9e85c8af5183317e59259
Rebecca <rebeccatmenezes@gmail.com>
2024-10-18 20:25:43 -0300

    commit

---
+++ admin - rebecca/admin.js
@@ -0,0 +1,13 @@
+function toggleTab() {
+    const content = document.getElementById('tabContent');
+    const icon = document.getElementById('rotateIcon');
+
+    // Alterna a visibilidade do conteúdo
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block'; // Mostra a aba
+        icon.classList.add('rotate'); // Adiciona a classe de rotação
+    } else {
+        content.style.display = 'none'; // Esconde a aba
+        icon.classList.remove('rotate'); // Remove a classe de rotação
+    }
+}
